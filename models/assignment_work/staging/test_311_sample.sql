199bc98dfb2dbe2a74aa5f4b183a6d938be6bd46
Jose Burga <jose.burga@baruchmail.cuny.edu>
2026-03-29 19:07:59 +0000

    Create test_311_sample.sql model

---
+++ models/assignment_work/staging/test_311_sample.sql
@@ -0,0 +1,8 @@
+-- Quick test to verify source connection works
+select 
+    unique_key,
+    created_date,
+    complaint_type,
+    borough
+from {{ source("raw", "source_dot_service_requests_history") }}
+limit 10
