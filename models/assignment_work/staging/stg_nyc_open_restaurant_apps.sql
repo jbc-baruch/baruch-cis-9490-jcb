e3c87f8568092007cc745dae964edff91191dcca
Jose Burga <jose.burga@baruchmail.cuny.edu>
2026-04-30 00:04:36 +0000

    deleting old model

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    LENGTH(CAST(zip AS STRING)) AS zip_length,
-    zip,
-    COUNT(*) AS count
- FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
-WHERE zip IS NOT NULL
-  AND LENGTH(CAST(zip AS STRING)) != 5
-GROUP BY zip_length, zip
-ORDER BY count DESC
--- models/assignment_work/staging/test_311_sample.sql
+++
@@ -1,4 +0,0 @@
--- Quick test to verify source connection works
-select unique_key, created_date, complaint_type, borough
-from {{ source("raw", "source_dot_service_requests_history") }}
-LIMIT 10
